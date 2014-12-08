8ff05dad9dde656d84a13b841cd480cb7e139a07
erkancelik <erkancelik@hotmail.com.tr>
2014-12-09 00:00:32 +0200

    son hali

--- ArabamNeYakar/Entities/UserRole.cs
+++ ArabamNeYakar/Entities/UserRole.cs
@@ -8,8 +8,8 @@ namespace ArabamNeYakar.Entities
     public partial class UserRole
     {
         public int id { get; set; }
-        public Nullable<int> role_id { get; set; }
-        public Nullable<int> user_id { get; set; }
+        //public Nullable<int> role_id { get; set; }
+        //public Nullable<int> user_id { get; set; }
         public short version { get; set; }
         public Nullable<System.DateTime> updateDate { get; set; }
         public string updateUserName { get; set; }
