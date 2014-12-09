e553ca72438dd496c0eb05f15a684a359db90cb3
erkancelik <erkancelik@hotmail.com.tr>
2014-12-09 23:10:24 +0200

    Logon

---
+++ ArabamNeYakar/Controllers/LogOnController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArabamNeYakar.Controllers
+{
+    public class LogOnController : Controller
+    {
+        //
+        // GET: /LogOn/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
